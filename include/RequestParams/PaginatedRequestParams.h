f2648cb9945dab328fae557448339966790a3ec9
Oleg <Oleg@HOME-PC>
2025-02-25 22:57:04 +0300

    fix: removed warning

--- include/RequestParams/PaginatedRequestParams.h
+++ include/RequestParams/PaginatedRequestParams.h
@@ -12,12 +12,12 @@ template <typename Derived, typename Params>
 class PaginatedRequestParamsBuilder : public RequestParamsBuilder<Derived, Params> {
 public:
     Derived& setOffset(uint16_t offset) {
-        params_.offset = offset;
+        this->params_.offset = offset;
         return static_cast<Derived&>(*this);
     }
 
     Derived& setLimit(uint16_t limit) {
-        params_.limit = limit;
+        this->params_.limit = limit;
         return static_cast<Derived&>(*this);
     }
 };
